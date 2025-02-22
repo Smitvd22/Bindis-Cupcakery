3de0bc9c237936e0efa07f21f29d517ce74f321e
Smitvd22 <smitvdeoghare@gmail.com>
2025-02-22 12:14:02 +0530

    Changed react configurations

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()], // Remove tailwindcss() here
   server: {
     host: true,
-    //port: 5173, When not running with docker compose, this is the port which will be used in docker
     allowedHosts: ["front-end"]
   },
   resolve: {
@@ -16,4 +13,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
